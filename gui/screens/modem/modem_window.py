71191e970e69ee1b584278673adfad7c4c25811c
Anne <annemachinda@gmail.com>
2024-11-01 14:27:07 +0100

    stack code altered to allow hidden window

--- gui/screens/modem/modem_window.py
+++ gui/screens/modem/modem_window.py
@@ -49,9 +49,10 @@ class ModemWindow(Gtk.Window):
         main_box.pack_start(sidebar, False, False, 0)
         
 
-        # Create the stack to switch between views
-        stack = Gtk.Stack()
-        main_box.pack_start(stack, True, True, 0)
+        # Create the self.stack to switch between views
+        self.stack = Gtk.Stack()
+        # self.add(self.stack)
+        main_box.pack_start(self.stack, True, True, 0)
 
         # Sidebar content
         sidebar_top = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
@@ -66,7 +67,7 @@ class ModemWindow(Gtk.Window):
         home_modem_event_box = Gtk.EventBox()
         home_modem_event_box.add(home_modem_label)
         home_modem_event_box.set_events(Gdk.EventMask.BUTTON_PRESS_MASK)
-        home_modem_event_box.connect("button-press-event", self.on_home_modem_clicked, stack)
+        home_modem_event_box.connect("button-press-event", self.on_home_modem_clicked, self.stack)
         sidebar_top.pack_start(home_modem_event_box, False, False, 0)
 
         line = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
@@ -83,7 +84,7 @@ class ModemWindow(Gtk.Window):
         send_event_box = Gtk.EventBox()
         send_event_box.add(send_label)
         send_event_box.set_events(Gdk.EventMask.BUTTON_PRESS_MASK)
-        send_event_box.connect("button-press-event", self.on_send_clicked, stack)
+        send_event_box.connect("button-press-event", self.on_send_clicked, self.stack)
         sidebar_top.pack_start(send_event_box, False, False, 0)
 
         line = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
@@ -100,7 +101,7 @@ class ModemWindow(Gtk.Window):
         incoming_event_box = Gtk.EventBox()
         incoming_event_box.add(incoming_label)
         incoming_event_box.set_events(Gdk.EventMask.BUTTON_PRESS_MASK)
-        incoming_event_box.connect("button-press-event", self.on_incoming_clicked, stack)
+        incoming_event_box.connect("button-press-event", self.on_incoming_clicked, self.stack)
         
         line = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
         sidebar_top.pack_start(incoming_event_box, False, False, 0)
@@ -114,7 +115,7 @@ class ModemWindow(Gtk.Window):
         outgoing_event_box = Gtk.EventBox()
         outgoing_event_box.add(outgoing_label)
         outgoing_event_box.set_events(Gdk.EventMask.BUTTON_PRESS_MASK)
-        outgoing_event_box.connect("button-press-event", self.on_outgoing_clicked, stack)
+        outgoing_event_box.connect("button-press-event", self.on_outgoing_clicked, self.stack)
 
         line = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
         sidebar_top.pack_start(outgoing_event_box, False, False, 0)
@@ -128,7 +129,7 @@ class ModemWindow(Gtk.Window):
         failed_event_box = Gtk.EventBox()
         failed_event_box.add(failed_label)
         failed_event_box.set_events(Gdk.EventMask.BUTTON_PRESS_MASK)
-        failed_event_box.connect("button-press-event", self.on_failed_clicked, stack)
+        failed_event_box.connect("button-press-event", self.on_failed_clicked, self.stack)
 
         line = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
         sidebar_top.pack_start(failed_event_box, False, False, 0)
@@ -142,7 +143,7 @@ class ModemWindow(Gtk.Window):
         encrypted_event_box = Gtk.EventBox()
         encrypted_event_box.add(encrypted_label)
         encrypted_event_box.set_events(Gdk.EventMask.BUTTON_PRESS_MASK)
-        encrypted_event_box.connect("button-press-event", self.on_encrypted_clicked, stack)
+        encrypted_event_box.connect("button-press-event", self.on_encrypted_clicked, self.stack)
         
         sidebar_top.pack_start(encrypted_event_box, False, False, 0)
 
@@ -159,7 +160,7 @@ class ModemWindow(Gtk.Window):
         message_forwarding_event_box = Gtk.EventBox()
         message_forwarding_event_box.add(message_forwarding_label)
         message_forwarding_event_box.set_events(Gdk.EventMask.BUTTON_PRESS_MASK)
-        message_forwarding_event_box.connect("button-press-event", self.on_message_forwarding_clicked, stack)
+        message_forwarding_event_box.connect("button-press-event", self.on_message_forwarding_clicked, self.stack)
         
         line = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
         sidebar_top.pack_start(message_forwarding_event_box, False, False, 0)
@@ -173,7 +174,7 @@ class ModemWindow(Gtk.Window):
         export_event_box = Gtk.EventBox()
         export_event_box.add(export_label)
         export_event_box.set_events(Gdk.EventMask.BUTTON_PRESS_MASK)
-        export_event_box.connect("button-press-event", self.on_export_clicked, stack)
+        export_event_box.connect("button-press-event", self.on_export_clicked, self.stack)
         
         line = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
         sidebar_top.pack_start(export_event_box, False, False, 0)
@@ -187,12 +188,27 @@ class ModemWindow(Gtk.Window):
         about_event_box = Gtk.EventBox()
         about_event_box.add(about_label)
         about_event_box.set_events(Gdk.EventMask.BUTTON_PRESS_MASK)
-        about_event_box.connect("button-press-event", self.on_about_clicked, stack)
+        about_event_box.connect("button-press-event", self.on_about_clicked, self.stack)
         
         line = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
         sidebar_top.pack_start(about_event_box, False, False, 0)
 
-        self.initialize_views(stack)
+
+        # # gatewaysever
+        # gateway_server_window = Gtk.Label()
+        # about_label.set_text("Gateway Server")
+        # about_label.set_name("side_label")
+        # about_label.set_margin_bottom(8)
+        # about_label.set_margin_top(8)
+        # about_event_box = Gtk.EventBox()
+        # about_event_box.add(about_label)
+        # about_event_box.set_events(Gdk.EventMask.BUTTON_PRESS_MASK)
+        # about_event_box.connect("button-press-event", self.on_about_clicked, self.stack)
+        
+        # line = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
+        sidebar_top.pack_start(about_event_box, False, False, 0)
+
+        self.initialize_views()
 
 
     def new_msg_handler(self, message:Messaging, sim_imsi):
@@ -200,82 +216,92 @@ class ModemWindow(Gtk.Window):
         self.incoming_view.new_message_handler(self.modem_name)
 
 
-    def initialize_views(self, stack):
+    def initialize_views(self):
 
         # home modem view
         home_modem_view = HomeModemWindow(self.modem_properties, self.modem_name, self.modem_handler)
-        stack.add_named(home_modem_view, "home_modem")
+        self.stack.add_named(home_modem_view, "home_modem")
         
         # send view
         send_view = SendMessageWindow(self.modem_handler, self.modem_name)
-        stack.add_named(send_view, "send")
+        self.stack.add_named(send_view, "send")
 
         # incoming view
         self.incoming_view = IncomingMessageWindow( self.modem_name, self.modem_handler)
-        stack.add_named(self.incoming_view, "incoming")
+        self.stack.add_named(self.incoming_view, "incoming")
 
         # outgoing view
         outgoing_view = OutgoingMessageWindow( self.modem_name, self.modem_handler)
-        stack.add_named(outgoing_view, "outgoing")
+        self.stack.add_named(outgoing_view, "outgoing")
 
         # failed view
         failed_view = FailedMessageWindow()
-        stack.add_named(failed_view, "failed")
+        self.stack.add_named(failed_view, "failed")
 
         # encrypted view
         encrypted_view = EncryptedMessageWindow()
-        stack.add_named(encrypted_view, "encrypted")
+        self.stack.add_named(encrypted_view, "encrypted")
 
         # message forwarding view
-        message_forwarding_view = MessageForwardingWindow()
-        stack.add_named(message_forwarding_view, "message_forwarding")
+        # message_forwarding_view = MessageForwardingWindow(self.stack)
+        # self.stack.add_named(message_forwarding_view, "message_forwarding")
+
+        message_forwarding_window = MessageForwardingWindow(self.stack)
+        self.stack.add_named(message_forwarding_window, "message_forwarding")
+
+        # Assuming sidebar contains the message_forwarding_event_box
+        # Connect the sidebar button to self.on_message_forwarding_clicked
+        # message_forwarding_event_box = Gtk.EventBox()
+        # message_forwarding_event_box.connect("button-press-event", self.on_message_forwarding_clicked)
 
         # export view
         export_view = ExportMessageWindow()
-        stack.add_named(export_view, "export")
+        self.stack.add_named(export_view, "export")
 
         # about view
         about_view = AboutWindow(self.modem_properties, self.modem_name, self.modem_handler)
-        stack.add_named(about_view, "about")
+        self.stack.add_named(about_view, "about")
 
 
 
-    def on_home_modem_clicked(self, widget, event, stack):
-        stack.set_visible_child_name("home_modem")
+    def on_home_modem_clicked(self, widget, event, *args):
+        self.stack.set_visible_child_name("home_modem")
 
-    def on_send_clicked(self, widget, event, stack):
-        stack.set_visible_child_name("send")
+    def on_send_clicked(self, widget, event, *args):
+        self.stack.set_visible_child_name("send")
         self.set_active_label("send_label")
 
-    def on_incoming_clicked(self, widget, event, stack):
-        stack.set_visible_child_name("incoming")
+    def on_incoming_clicked(self, widget, event, *args):
+        self.stack.set_visible_child_name("incoming")
         self.set_active_label("incoming_label")
 
-    def on_outgoing_clicked(self, widget, event, stack):
-        stack.set_visible_child_name("outgoing")
+    def on_outgoing_clicked(self, widget, event, *args):
+        self.stack.set_visible_child_name("outgoing")
         self.set_active_label("outgoing_label")
 
-    def on_failed_clicked(self, widget, event, stack):
-        stack.set_visible_child_name("failed")
+    def on_failed_clicked(self, widget, event, *args):
+        self.stack.set_visible_child_name("failed")
         self.set_active_label("failed_label")
    
-    def on_encrypted_clicked(self, widget, event, stack):
-        stack.set_visible_child_name("encrypted")
+    def on_encrypted_clicked(self, widget, event, *args):
+        self.stack.set_visible_child_name("encrypted")
         self.set_active_label("encrypted_label")
 
-    def on_message_forwarding_clicked(self, widget, event, stack):
-        stack.set_visible_child_name("message_forwarding")
+    def on_message_forwarding_clicked(self, widget, event, *args):
+
+        print(f"Arguments received in on_message_forwarding_clicked: {args}")
+        self.stack.set_visible_child_name("message_forwarding")
         self.set_active_label("message_forwarding_label")
 
-    def on_export_clicked(self, widget, event, stack):
-        stack.set_visible_child_name("export")
+    def on_export_clicked(self, widget, event, *args):
+        self.stack.set_visible_child_name("export")
         self.set_active_label("export_label")
 
-    def on_about_clicked(self, widget, event, stack):
-        stack.set_visible_child_name("about")
+    def on_about_clicked(self, widget, event, *args):
+        self.stack.set_visible_child_name("about")
         self.set_active_label("about_label")
 
-    def on_test_clicked(self, widget, event, stack):
+    def on_test_clicked(self, widget, event, *args):
         popover = Gtk.Popover.new_from_widget(widget)
         popover.set_position(Gtk.PositionType.BOTTOM)
         
